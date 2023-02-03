1b3b632a27df5653f5f0c5b9710df397cda00456
ahmadullahtonmoy <ahmadullahtonmoy@gmail.com>
2023-02-04 01:41:08 +0600

    rainbow 0.1 projects materials added

---
+++ colors.js
@@ -0,0 +1,29 @@
+// bg colors for body
+// red
+function makeRed() {
+    document.body.style.backgroundColor="red";
+}
+// blue
+function makeBlue() {
+    document.body.style.backgroundColor="blue";
+}
+// yellow
+function makeYellow() {
+    document.body.style.backgroundColor="yellow";
+}
+// green
+function makeGreen() {
+    document.body.style.backgroundColor="green";
+}
+// purple
+function makePurple() {
+    document.body.style.backgroundColor="purple";
+}
+// pink
+function makePink() {
+    document.body.style.backgroundColor="pink";
+}
+// White
+function makeWhite() {
+    document.body.style.backgroundColor="white";
+}
